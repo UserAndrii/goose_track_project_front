e8b8f4deaf8522a735413a8dc63a1d49bc639c99
GalynkaK <gkarpinskaya@gmail.com>
2023-08-27 00:11:07 +0300

    ceated 404 page

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,64 @@
+import {
+  Text,
+  Number,
+  Wrapper,
+  Container,
+  Picture,
+  Link,
+} from "./NotFound.styled";
+import images from "./images";
+
+const { phone, tablet, desktop } = images;
+
+export const NotFound = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Number>4</Number>
+        <Picture>
+          <source
+            type="image/webp"
+            media="(max-width: 767px)"
+            srcSet={`${phone.notFoundPhonewebp} 1x, ${phone.notFoundPhonewebp2x} 2x`}
+          />
+          <source
+            type="image/webp"
+            media="(max-width: 1279px)"
+            srcSet={`${tablet.notFoundTabletwebp} 1x, ${tablet.notFoundTabletwebp2x} 2x`}
+          />
+          <source
+            type="image/webp"
+            media="(min-width: 1280px)"
+            srcSet={`${desktop.notFoundDesktopwebp} 1x, ${desktop.notFoundDesktopwebp2x} 2x`}
+          />
+          <source
+            type="image/png"
+            media="(max-width: 767px)"
+            srcSet={`${phone.notFoundPhonepng} 1x, ${phone.notFoundPhonepng2x} 2x`}
+          />
+          <source
+            type="image/png"
+            media="(max-width: 1279px)"
+            srcSet={`${tablet.notFoundTabletpng} 1x, ${tablet.notFoundTabletpng2x} 2x`}
+          />
+          <source
+            type="image/png"
+            media="(min-width: 1280px)"
+            srcSet={`${desktop.notFoundDesktoppng} 1x, ${desktop.notFoundDesktoppng2x} 2x`}
+          />
+          <img
+            src={`${phone.notFoundPhonepng}`}
+            alt="A rocket with the goose instead zero"
+          />
+        </Picture>
+        <Number>4</Number>
+      </Wrapper>
+      <Text>
+        We're sorry, the page <Link to={"/"}>homepage</Link>.
+      </Text>
+      <StyledButton onClick={() => navigate('/')}>
+        Back to home
+      </StyledButton>
+    </Container>
+  );
+};
---
+++ src/components/NotFound/images.js
@@ -0,0 +1,43 @@
+import notFoundPhonepng from "../../images/notFound/png/phone/notFound-min-p.png";
+import notFoundPhonepng2x from "../../images/notFound/png/phone/notFound@2x-min-p.png";
+
+import notFoundPhonewebp from "../../images/notFound/webp/phone/notFound-min-p.webp";
+import notFoundPhonewebp2x from "../../images/notFound/webp/phone/notFound@2x-min-p.webp";
+
+import notFoundTabletpng from "../../images/notFound/png/tablet/notFound-min-t.png";
+import notFoundTabletpng2x from "../../images/notFound/png/tablet/notFound@2x-min-t.png";
+
+import notFoundTabletwebp from "../../images/notFound/webp/tablet/notFound-min-t.webp";
+import notFoundTabletwebp2x from "../../images/notFound/webp/tablet/notFound@2x-min-t.webp";
+
+import notFoundDesktoppng from "../../images/notFound/png/desktop/notFound-min.png";
+import notFoundDesktoppng2x from "../../images/notFound/png/desktop/notFound@2x-min.png";
+
+import notFoundDesktopwebp from "../../images/notFound/webp/desktop/notFound-min-d.webp";
+import notFoundDesktopwebp2x from "../../images/notFound/webp/desktop/notFound@2x-min-d.webp";
+
+const images = {
+  phone: {
+    notFoundPhonepng,
+    notFoundPhonepng2x,
+
+    notFoundPhonewebp,
+    notFoundPhonewebp2x,
+  },
+  tablet: {
+    notFoundTabletpng,
+    notFoundTabletpng2x,
+
+    notFoundTabletwebp,
+    notFoundTabletwebp2x,
+  },
+  desktop: {
+    notFoundDesktoppng,
+    notFoundDesktoppng2x,
+
+    notFoundDesktopwebp,
+    notFoundDesktopwebp2x,
+  },
+};
+
+export default images;
