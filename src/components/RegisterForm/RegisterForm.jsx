1f25d90fc2eab4914d2309d3f907a26ae18de2a9
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2023-08-26 22:22:06 +0200

    some adds

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import { StyledForm, InputGroupe, LinksContainer, FormName, InputWrapper, ButtonSubmit} from './RegisterForm.styled';
+import {
+  StyledForm,
+  InputGroupe,
+  LinksContainer,
+  FormName,
+  InputWrapper,
+  Button,
+  ButtonText,
+  ItemIcon,
+  Label,
+  Input,
+  ShowHideButton,
+  InputWrapperWithIcon,
+  InputList,
+} from './RegisterForm.styled';
 // import { showErrorToast, showSuccessToast } from '../../utils/messages';
 import { Link } from 'react-router-dom';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+import { useState } from 'react';
 
 const RegisterForm = () => {
-  // const emailRegexp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  const [showPassword, setShowPassword] = useState(false);
+  const emailRegexp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
   const validationSchema = yup.object({
-    name: yup.string()
-    .min(3, 'Name must be at least 3 characters')
-    .required('This field is required'),
-    email: yup.string()
-    // .matches(emailRegexp, 'Invalid email')
-    .required('This field is required'),
-    password: yup.string()
-    .min(6, 'Password must be at least 6 characters')
-    .required('This field is required'),
+    name: yup
+      .string()
+      .min(3, 'Name must be at least 3 characters')
+      .required('This field is required'),
+    email: yup
+      .string()
+      .matches(emailRegexp, 'Invalid email')
+      .required('This field is required'),
+    password: yup
+      .string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('This field is required'),
   });
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     event.preventDefault();
     const { name, value } = event.currentTarget;
 
@@ -52,7 +72,7 @@ const RegisterForm = () => {
       password: '',
     },
     validationSchema: validationSchema,
-    onSubmit: async (values) => {
+    onSubmit: async values => {
       try {
         const formData = new FormData();
         formData.append('name', values.name);
@@ -83,63 +103,78 @@ const RegisterForm = () => {
 
   return (
     <StyledForm
-    //  onSubmit={formik.handleSubmit}
-     >
+     onSubmit={formik.handleSubmit}
+    >
       <InputGroupe>
         <FormName>Sign Up</FormName>
-        <ul>
+        <InputList>
           <InputWrapper>
-            <label htmlFor="name">Name:</label>
-            <input
+            <Label htmlFor="name">Name</Label>
+            <Input
               type="text"
               id="name"
               name="name"
+              autoComplete='true'
               value={formik.values.name}
-              placeholder='Enter your name'
+              placeholder="Enter your name"
               onChange={handleChange}
+  onBlur={formik.handleBlur}
             />
-            {formik.touched.name && formik.errors.name && (
-              <div className="error">{formik.errors.name}</div>
-            )}
+            {formik.errors.name && formik.touched.name && (
+  <div className="error">{formik.errors.name}</div>
+)}
           </InputWrapper>
-          <InputWrapper className="input-group">
-            <label htmlFor="email">Email:</label>
-            <input
+          <InputWrapper>
+            <Label htmlFor="email">Email</Label>
+            <Input
               type="text"
               id="email"
               name="email"
+              autoComplete='true'
               value={formik.values.email}
-              placeholder='Enter email'
+              placeholder="Enter email"
               onChange={handleChange}
             />
-            {formik.touched.email && formik.errors.email && (
-              <div className="error">{formik.errors.email}</div>
-            )}
+            {formik.errors.email && formik.touched.email && (
+  <div className="error">{formik.errors.email}</div>
+)}
           </InputWrapper>
-          <InputWrapper className="input-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formik.values.password}
-              placeholder='Enter password'
-              onChange={handleChange}
-            />
-            {formik.touched.password && formik.errors.password && (
-              <div className="error">{formik.errors.password}</div>
-            )}
+          <InputWrapper>
+            <Label htmlFor="password">Password</Label>
+            <InputWrapperWithIcon>
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                name="password"
+                value={formik.values.password}
+                placeholder="Enter password"
+                onChange={handleChange}
+              />
+              <ShowHideButton
+                type="button"
+                onClick={() => setShowPassword(show => !show)}
+              >
+                {showPassword ? <FiEyeOff /> : <FiEye />}
+              </ShowHideButton>
+              {formik.touched.password && formik.errors.password && (
+                <div className="error">{formik.errors.password}</div>
+              )}
+            </InputWrapperWithIcon>
+
+            {formik.errors.password && formik.touched.password && (
+  <div className="error">{formik.errors.password}</div>
+)}
           </InputWrapper>
-        </ul>
+        </InputList>
+        <Button type='submit'>
+          <ButtonText>Sign Up</ButtonText>
+          <ItemIcon />
+        </Button>
       </InputGroupe>
-      <ButtonSubmit type="submit">Sign Up</ButtonSubmit>
-      <LinksContainer>
-        <span>
-          <Link to="/">Forgot password?</Link>
-        </span> <br/>
-        <span>Already registered? 
+      <LinksContainer><span>Forgot password?</span>
+          <Link to="/">change password</Link>
+          <span>Already registered?</span>
           <Link to="/login">Log In</Link>
-        </span>
       </LinksContainer>
     </StyledForm>
   );
--- src/components/RegisterForm/RegisterForm.styled.jsx
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
+import { FiLogIn } from 'react-icons/fi';
 
 export const StyledForm = styled.form`
-  display: inline-flex;
+  display: flex;
 padding: 155px 20px 156px 20px;
 flex-direction: column;
 justify-content: center;
@@ -18,23 +19,35 @@ font-weight: 600;
 line-height: 133.333% ;
 `;
 
-export const InputGroupe = styled.li`
-  
+export const InputGroupe = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+justify-content: center;
+gap: 32px;
+  padding: 40px 24px;
+  border-radius: 8px;
+background: #FFF;
+
 `;
+
+export const InputList = styled.ul`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+gap: 24px;
+background: #FFF;
+
+`;
+
 export const InputWrapper = styled.li`
   display: flex;
   flex-direction: column;
 
-  label {
-    margin-bottom: 0.25rem;
-  }
 
-  input {
-    padding: 0.5rem;
-  }
 
-  input::placeholder {
-    color: #DCE3E5;
+  Input::placeholder {
+    color: #858688;
     font-family: Inter;
     font-size: 14px;
     font-style: normal;
@@ -43,40 +56,93 @@ export const InputWrapper = styled.li`
   }
 
   @media (min-width: 768px) {
-    flex-direction: row;
-    align-items: center;
-
-    label {
-      margin-bottom: 0;
-      margin-right: 1rem;
-    }
-
-    input {
-      padding: 0.5rem;
-      width: 100%;
-    }
+    
   }
 `;
-export const ButtonSubmit = styled.button`
+
+export const InputWrapperWithIcon = styled.div`
   display: flex;
-width: 287px;
-padding: 14px 103px;
 flex-direction: column;
-align-items: center;
+align-items: flex-start;
+gap: 8px;
+position: relative;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+  margin: auto;
+  width: 287px;
+padding: 14px 103px;
 gap: 10px;
 border-radius: 16px;
 background: #3E85F3;
 box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
-` 
+cursor: pointer;
+` ;
 
-export const LinksContainer = styled.div`
-  margin-bottom: 1rem;
+export const ButtonText = styled.span`
+    white-space: nowrap;
+    color: #FFFFFF;
+    font-family: inherit;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 1.29;
+    letter-spacing: -0.28px;
+`;  
+export const ItemIcon = styled(FiLogIn)`
+    width: 18px;
+    height: 18px;
+    flex-shrink: 0;
+    color: #FFFFFF;
+`;
+
+export const ShowHideButton = styled.button`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+`;
 
+export const LinksContainer = styled.nav`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-top: 18px;
+  gap: 18px;
   @media (max-width: 768px) {
-    flex-direction: column;
-    align-items: flex-start;
   }
 `;
+
+
+export const Label = styled.label`
+margin-bottom: 8px;
+font-family: Inter;
+font-size: 12px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+color: #111111;
+`;
+
+export const Input = styled.input`
+width: 287px;
+height: 46px;
+border-radius: 8px;
+border: 1px solid rgba(220, 227, 229, 0.60);
+background: #FFFFFF;
+background: ${props => (props.isPassword ? '#FFF' : 'inherit')};
+`;
+export const ViewSVG = styled.button`
+
+`;
+
