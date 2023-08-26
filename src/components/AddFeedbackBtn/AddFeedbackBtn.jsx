bf5d2184f860342a0427f9ef9494c7b1c42cef42
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-08-26 22:52:19 +0200

    Update AddFeedbackBtn.jsx

--- src/components/AddFeedbackBtn/AddFeedbackBtn.jsx
+++ src/components/AddFeedbackBtn/AddFeedbackBtn.jsx
@@ -1,7 +1,15 @@
 import { Button } from './AddFeedbackBtn.styled';
 
+<<<<<<< Updated upstream
 const AddFeedbackBtn = ({ onClick }) => {
   return <Button onClick={onClick}>Feedback</Button>;
+=======
+const AddFeedbackBtn = ({ isModalOpen = false}) => {
+  return<div>
+<Button onClick={isModalOpen=true}>Feedback</Button>
+{isModalOpen  && <div>qqq</div>}
+  </div> 
+>>>>>>> Stashed changes
 };
 
 export default AddFeedbackBtn;
