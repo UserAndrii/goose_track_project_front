f8383f2d4d53d289a4aae923d8ff2d7e756f94d1
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2023-08-26 12:54:25 +0200

    some adds

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import { StyledForm, InputGroupe } from './RegisterForm.styled';
-import { showErrorToast, showSuccessToast } from '../../utils/messages';
+import { StyledForm, InputGroupe, LinksContainer, FormName, InputWrapper, ButtonSubmit} from './RegisterForm.styled';
+// import { showErrorToast, showSuccessToast } from '../../utils/messages';
+import { Link } from 'react-router-dom';
 
 const RegisterForm = () => {
-  const emailRegexp =
-    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  const emailRegexp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
   const validationSchema = yup.object({
-    name: yup.string().min(3, 'Name must be at least 3 characters').required('This field is required'),
-    email: yup.string().matches(emailRegexp, 'Invalid email').required('This field is required'),
-    password: yup.string().min(6, 'Password must be at least 6 characters').required('This field is required'),
+    name: yup.string()
+    .min(3, 'Name must be at least 3 characters')
+    .required('This field is required'),
+    email: yup.string()
+    // .matches(emailRegexp, 'Invalid email')
+    .required('This field is required'),
+    password: yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('This field is required'),
   });
 
   const handleChange = (event) => {
@@ -76,57 +82,65 @@ const RegisterForm = () => {
   });
 
   return (
-    <StyledForm onSubmit={formik.handleSubmit}>
-      <ul>
-        <InputGroupe className="input-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formik.values.name}
-            onChange={handleChange}
-          />
-          {formik.touched.name && formik.errors.name && (
-            <div className="error">{formik.errors.name}</div>
-          )}
-        </InputGroupe>
-        <InputGroupe className="input-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={formik.values.email}
-            onChange={handleChange}
-          />
-          {formik.touched.email && formik.errors.email && (
-            <div className="error">{formik.errors.email}</div>
-          )}
-        </InputGroupe>
-        <InputGroupe className="input-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formik.values.password}
-            onChange={handleChange}
-          />
-          {formik.touched.password && formik.errors.password && (
-            <div className="error">{formik.errors.password}</div>
-          )}
-        </InputGroupe>
-      </ul>
-      <button type="submit">Sign Up</button>
-      <div className="links-container">
-          <span>
-            <a href="#">Forgot password?</a>
-          </span> <br/>
-          <span>Already registered? 
-            <a href="#">Log In</a>
-          </span>
-        </div>
+    <StyledForm
+    //  onSubmit={formik.handleSubmit}
+     >
+      <InputGroupe>
+        <FormName>Sign Up</FormName>
+        <ul>
+          <InputWrapper>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formik.values.name}
+              placeholder='Enter your name'
+              onChange={handleChange}
+            />
+            {formik.touched.name && formik.errors.name && (
+              <div className="error">{formik.errors.name}</div>
+            )}
+          </InputWrapper>
+          <InputWrapper className="input-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formik.values.email}
+              placeholder='Enter email'
+              onChange={handleChange}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <div className="error">{formik.errors.email}</div>
+            )}
+          </InputWrapper>
+          <InputWrapper className="input-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formik.values.password}
+              placeholder='Enter password'
+              onChange={handleChange}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <div className="error">{formik.errors.password}</div>
+            )}
+          </InputWrapper>
+        </ul>
+      </InputGroupe>
+      <ButtonSubmit type="submit">Sign Up</ButtonSubmit>
+      <LinksContainer>
+        <span>
+          <Link to="/">Forgot password?</Link>
+        </span> <br/>
+        <span>Already registered? 
+          <Link to="/login">Log In</Link>
+        </span>
+      </LinksContainer>
     </StyledForm>
   );
 };
