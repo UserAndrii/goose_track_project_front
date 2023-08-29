179360cb145ad9b7d19d1f2982737de94f0b5544
Oderen~ <oleksandr.deren02@gmail.com>
2023-08-29 22:45:25 +0300

    calendar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-// import { lazy } from 'react';
+import { lazy } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import MainLayout from './MainLayout/MainLayout';
@@ -21,10 +21,10 @@ import StatisticsPage from 'pages/StatisticsPage';
 export const App = () => {
   return (
     <>
-      {/* <TemporaryNavigation /> */}
+      <TemporaryNavigation />
       <Routes>
         <Route path="/" element={<CalendarPage />} />
-        {/* <Route
+        <Route
           path="/register"
           element={
             <RestrictedRoute
@@ -68,8 +68,8 @@ export const App = () => {
 
         <Route
           path="*"
-          element={<RestrictedRoute component={<MainPage />} navigateTo="/" />} */}
-        {/* /> */}
+          element={<RestrictedRoute component={<MainPage />} navigateTo="/" />}
+        />
       </Routes>
     </>
   );
