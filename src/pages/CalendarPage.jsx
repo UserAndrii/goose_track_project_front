5ee46fc43438dbf089cc5260ea597be39d1b25b9
Oderen~ <oleksandr.deren02@gmail.com>
2023-08-30 11:03:15 +0300

    calendar

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,15 +1,16 @@
-import AddFeedbackBtn from 'components/AddFeedbackBtn/AddFeedbackBtn';
+// import AddFeedbackBtn from 'components/AddFeedbackBtn/AddFeedbackBtn';
 import { useState, useEffect } from 'react';
 import AddTaskModal from 'components/AddTaskModal/AddTaskModal';
-import TasksColumnsList from 'components/TasksColumnsList/TasksColumnsList';
+// import TasksColumnsList from 'components/TasksColumnsList/TasksColumnsList';
+import { Calendar } from '../components/Calendar/Calendar';
 
 const CalendarPage = () => {
   const [isModalOpen, setModalOpen] = useState(false);
 
-  const handleOpenModal = () => {
-    setModalOpen(true);
-    document.body.style.overflow = 'hidden';
-  };
+  // const handleOpenModal = () => {
+  //   setModalOpen(true);
+  //   document.body.style.overflow = 'hidden';
+  // };
 
   const handleCloseModal = () => {
     setModalOpen(false);
@@ -34,9 +35,8 @@ const CalendarPage = () => {
 
   return (
     <div>
-      <TasksColumnsList />
-      <AddFeedbackBtn onClick={handleOpenModal}>Add Task</AddFeedbackBtn>
-      {/* <h1>This is CalendarPage</h1> */}
+      <Calendar />
+      {/* <TasksColumnsList /> */}
       {isModalOpen && <AddTaskModal onClose={handleCloseModal} />}
     </div>
   );
