50303f96547b523dbe2f3eb24558fed9e2ecc993
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-08-31 22:52:08 +0200

    FeedBAckModaRRREEEADY...almost

--- src/components/FeedbackForm/FeedbackForm.styled.jsx
+++ src/components/FeedbackForm/FeedbackForm.styled.jsx
@@ -1,9 +1,15 @@
 import styled from '@emotion/styled';
+import {VscEdit} from 'react-icons/vsc'
+import {RiDeleteBin6Line} from 'react-icons/ri'
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
-  padding: 32px;
+  padding: 12px;
+  @media (min-width: 768px){
+    width: 438px;
+    height: 269px;
+  }
 `;
 
 export const RatingArea = styled.div`
@@ -20,11 +26,16 @@ export const Input = styled.input`
 export const RatingWrapper = styled.div`
 display: flex;
 flex-direction: column;
+
 `;
 
 export const TextAreaLabel = styled.label`
   display: flex;
-  flex-direction: column;
+  margin-top: 24px;
+  flex-direction: row;
+    align-items: center;
+    margin-top: 24px;
+    justify-content: space-between;
 `;
 
 export const TextArea = styled.textarea`
@@ -48,9 +59,11 @@ export const TextArea = styled.textarea`
 export const BtnWrapper = styled.div`
   display: flex;
   gap: 8px;
-  @media (max-width: 767px)
-  display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  @media (max-width: 767px){
+    display: flex;
+    flex-direction: column;
+  }
 }
 `;
 
@@ -74,4 +87,37 @@ export const Btn = styled.button`
     color: #fff;
     background-color: #3e85f3;
   }
+`;
+
+export const IconContainer = styled.div`
+display: flex;
+gap: 8px;
+`;
+
+export const StyledEditIcon = styled(VscEdit)`
+  fill: rgba(62, 133, 243, 1);
+  display: flex;
+  width: 30px;
+  height: 30px;
+  padding: 7px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  background: #E3F3FF;
+  cursor: pointer;
+`;
+
+export const StyledEditIconDel = styled(RiDeleteBin6Line)`
+  fill: rgba(234, 61, 101, 1);
+  display: flex;
+  width: 30px;
+  cursor: pointer;
+  height: 30px;
+  background: rgba(234, 61, 101, 0.20);
+  padding: 7px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
 `;
