426eccef9eac8b5b86b4ce8df5b2dd8c2c893e95
Andrii Gadar <gadar.andre@gmail.com>
2023-09-01 03:35:42 +0300

    added lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-// import { lazy } from 'react';
+import { lazy } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
@@ -14,25 +14,17 @@ import { getCurrentUser } from 'redux/auth/operations';
 import Spiner from './Spiner/Spiner';
 import MainLayout from './MainLayout/MainLayout';
 
-import MainPage from 'pages/MainPage';
-import RegisterPage from 'pages/RegisterPage';
-import LoginPage from 'pages/LoginPage';
-import AccountPage from 'pages/AccountPage';
-import CalendarPage from 'pages/CalendarPage';
-import StatisticsPage from 'pages/StatisticsPage';
-import NotFoundPage from 'pages/NotFoundPage';
-import TeamPage from 'pages/TeamPage';
 import { ChoosedMonth } from './Calendar/ChoosedMonth/ChoosedMonth';
 import { ChoosedDay } from './Calendar/ChoosedDay/ChoosedDay';
 
-// const MainPage = lazy(() => import('pages/MainPage'));
-// const RegisterPage = lazy(() => import('pages/RegisterPage'));
-// const LoginPage = lazy(() => import('pages/LoginPage'));
-// const AccountPage = lazy(() => import('pages/AccountPage'));
-// const CalendarPage = lazy(() => import('pages/CalendarPage'));
-// const StatisticsPage = lazy(() => import('pages/StatisticsPage'));
-// const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
-// const TeamPage = lazy(() => import('pages/TeamPage'));
+const MainPage = lazy(() => import('pages/MainPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const AccountPage = lazy(() => import('pages/AccountPage'));
+const CalendarPage = lazy(() => import('pages/CalendarPage'));
+const StatisticsPage = lazy(() => import('pages/StatisticsPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
+const TeamPage = lazy(() => import('pages/TeamPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
