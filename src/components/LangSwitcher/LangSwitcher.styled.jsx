c6e5f8e6d34b0c7c26c59ad81727d01ddc99500e
annatill <anna.tataurova.95@gmail.com>
2023-09-02 21:24:27 +0200

    Button style

---
+++ src/components/LangSwitcher/LangSwitcher.styled.jsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  @media screen and (max-width: 767px) {
+    > :nth-child(1) {
+        margin-right: 10px;
+  }
+`;
+
+export const Button = styled.button`
+  background: #ffffff;
+  display: flex;
+  align-items: center;
+  border: none;
+  border-radius: 16px;
+  box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+
+  color: #3e85f3;
+  font-family: inherit;
+  font-size: 10px;
+  font-weight: 600;
+  line-height: 1.29;
+  letter-spacing: -0.28px;
+
+  padding: 5px 15px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    padding: 10px 20px;
+    margin-left: 24px;
+    margin-bottom: 0;
+  }
+`;
